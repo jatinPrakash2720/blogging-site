be5cf3132e376272afbd5bcfa06c5e62da448d8e
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-07-26 12:40:27 +0530

    Remove commented-out express server setup code from index.js to clean up the file and improve readability.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,20 +5,4 @@ dotenv.config({
 });
 
 connectDB();
-// import express from "express";
-// const app = express();
-// ; (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("ERROR: |APP| :", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("ERROR: |MONGOOSE| : ", error);
-//     throw error;
-//   }
-// })();
+
