0c1ec330a51e3953b460561782aea3cfa5ab8f35
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-08-17 01:22:08 +0530

    Remove deprecated files and configurations: Deleted unused files including .gitignore, ESLint configuration, index.html, package.json, and various API and context files related to user, blog, and category management. This cleanup streamlines the project structure and removes legacy code that is no longer needed.

--- app/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import { LocalStorage } from "./LocalStorage.util";
-import { requestHandler } from "./requestHander.util";
-
-const isBrowser = typeof window !== "undefined";
-const classNames = (...className) => {
-  return className.filter(Boolean).join(" ");
-};
-
-export {LocalStorage,requestHandler,isBrowser,classNames}
